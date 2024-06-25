80aed30840c6b4922f3910f5c097761ef7f411ca
daniele arosio <daniele.arosio@cnr.it>
2024-06-25 14:52:49 +0200

    refactor: Use placeholder and posthook in pre-commit-config

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -33,3 +33,51 @@ for path_str in remove_paths:
             path.unlink()
         elif path.is_dir():
             shutil.rmtree(path)
+
+
+def modify_pre_commit_config():
+    project_dir = Path.cwd()
+    config_path = project_dir / ".pre-commit-config.yaml"
+    # Read the current content of the file
+    content = config_path.read_text()
+    # Prepare the replacement content based on project type
+    project_type = "{{ cookiecutter.project_type }}"
+    if project_type == "Python Project":
+        python_hooks = "- id: check-symlinks"
+        commitizen_hook = "- id: commitizen-branch\n        stages: [push]"
+        analysis_exclude = ""
+    elif project_type == "Data Analysis Project":
+        python_hooks = ""
+        commitizen_hook = ""
+        analysis_exclude = "exclude: ^data/.*\n"
+    else:
+        python_hooks = ""
+        commitizen_hook = ""
+        analysis_exclude = ""
+    # Perform replacements and remove lines if they should be empty
+    lines = content.splitlines()
+    new_lines = []
+    for line in lines:
+        if "# OPTIONAL_PYTHON_PROJECT_HOOKS" in line:
+            if python_hooks:
+                new_lines.append(
+                    line.replace("# OPTIONAL_PYTHON_PROJECT_HOOKS", python_hooks)
+                )
+        elif "# OPTIONAL_COMMITIZEN_BRANCH" in line:
+            if commitizen_hook:
+                new_lines.append(
+                    line.replace("# OPTIONAL_COMMITIZEN_BRANCH", commitizen_hook)
+                )
+        elif "# DATA_ANALYSIS_EXCLUDE" in line:
+            if analysis_exclude:
+                new_lines.append(
+                    line.replace("# DATA_ANALYSIS_EXCLUDE", analysis_exclude)
+                )
+        else:
+            new_lines.append(line)
+    # Write the new content back to the file
+    config_path.write_text("\n".join(new_lines) + "\n")
+
+
+# Execute function
+modify_pre_commit_config()
