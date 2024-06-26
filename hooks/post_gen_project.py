a24ba7dd9d3d089b468cf2e449423ca3280597b4
daniele arosio <daniele.arosio@cnr.it>
2024-06-26 15:48:02 +0200

    refactor: Use placeholder and posthook in pyproject.toml

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,7 +3,7 @@
 import shutil
 from pathlib import Path
 
-shutil.move("project.toml", "pyproject.toml")
+# shutil.move("project.toml", "pyproject.toml")
 
 project_type = "{{ cookiecutter.project_type }}"
 if project_type == "Data Analysis Project":
@@ -81,5 +81,63 @@ def modify_pre_commit_config():
     config_path.write_text("\n".join(new_lines) + "\n")
 
 
+def modify_pyproject():
+    project_dir = Path.cwd()
+    config_path = project_dir / "pyproject.toml"
+    # Read the current content of the file
+    content = config_path.read_text()
+    # Prepare the replacement content based on project type
+    project_type = "{{ cookiecutter.project_type }}"
+    if project_type == "Python Project":
+        readme_md = 'readme = "README.md"'
+        project_urls = """[project.urls]
+"Bug Tracker" = "https://github.com/darosio/{{ cookiecutter.project_name }}/issues"
+Changelog = "https://github.com/darosio/{{ cookiecutter.project_name }}/blob/main/CHANGELOG.md"
+Discussions = "https://github.com/darosio/{{ cookiecutter.project_name }}/discussions"
+Documentation = "https://{{ cookiecutter.project_slug }}.readthedocs.io"
+"Github releases" = "https://github.com/darosio/{{ cookiecutter.project_name }}/releases"
+Homepage = "https://github.com/darosio/{{ cookiecutter.project_name }}"
+"""
+        version_in_readme = '"README.md:Version"'
+        message_template = '{% raw %}message_template = "{{change_type}}:{% if show_message %} {{message}}{% endif %}"{% endraw %}'
+        hatch_bump = ',\n  "hatch build",\n  "export TEST_PYPI_TOKEN=$(pass show cloud/test_pypi | head -n 1) && hatch publish -r test -u __token__ -a $TEST_PYPI_TOKEN"'
+    elif project_type == "Data Analysis Project":
+        readme_md = ""
+        project_urls = ""
+        version_in_readme = '"Readme.org:Version"'
+        message_template = '{% raw %}message_template = "{{change_type}}:{% if show_message %} {{message}}{% endif %}"{% endraw %}'
+        hatch_bump = ""
+    else:
+        readme_md = ""
+        project_urls = ""
+        version_in_readme = ""
+        message_template = ""
+        hatch_bump = ""
+    # Perform replacements and remove lines if they should be empty
+    lines = content.splitlines()
+    new_lines = []
+    for line in lines:
+        if "# README_MD" in line:
+            if readme_md:
+                new_lines.append(line.replace("# README_MD", readme_md))
+        elif "# PROJECT_URLS" in line:
+            if project_urls:
+                new_lines.append(line.replace("# PROJECT_URLS", project_urls))
+        elif "# VERSION_IN_README" in line:
+            if version_in_readme:
+                new_lines.append(line.replace("# VERSION_IN_README", version_in_readme))
+        elif "# HATCH_BUMP" in line:
+            if hatch_bump:
+                new_lines.append(line.replace("  # HATCH_BUMP", hatch_bump))
+        elif "# MESSAGE_TEMPLATE" in line:
+            if message_template:
+                new_lines.append(line.replace("# MESSAGE_TEMPLATE", message_template))
+        else:
+            new_lines.append(line)
+    # Write the new content back to the file
+    config_path.write_text("\n".join(new_lines) + "\n")
+
+
 # Execute function
 modify_pre_commit_config()
+modify_pyproject()
